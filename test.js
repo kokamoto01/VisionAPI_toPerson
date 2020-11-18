761ce259ced20e4f4cde6cf5caac38687b856582
kokamoto01 <k017c1355@it-neec.jp>
2020-11-18 14:02:23 +0900

    外部からAPIキーが見えてしまう問題を修正

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var {PythonShell} = require('python-shell');
 var options = {
   mode: 'text', // testもしくはjson
   pythonPath: '/usr/local/bin/python3', // Python3のパスを指定しないと動かないので注意
-  args: ['AIzaSyCLp2_-BzLz1hozOAuqXlyvhi5iR8BzaGY', 'test02.jpg'] // 引数を設定
+  args: ['AIzaSyCLp2_-BzLz1hozOAuqXlyvhi5iR8BzaGY', 'test03.png'] // 引数を設定
 };
 
 var pyshell = new PythonShell('./test.py',options);
